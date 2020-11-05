cc184ea19c62c4680a7d9e76ffb8ba455d276b1f
Alexander Koc <alexander.koc@slu.se>
2020-11-05 11:45:06 +0100

    Add dog study

---
+++ R/dog_study.R
@@ -0,0 +1,20 @@
+#' Dog parameters
+#'
+#' Data from infamous dog breeding experiment
+#'
+#' @docType data
+#'
+#' @usage data(dog_study)
+#'
+#' @format An object of class \code{"tibble"}
+#'
+#' @keywords datasets
+#'
+#' @references L'bardo et al. (2022) Dog Genetics 231:177-216
+#' (\href{https://external-preview.redd.it/3IKvKF2xhJOwsggp_N3HwklJogvj4vG0DgdHircthj4.jpg?auto=webp&s=18c64cd8eab1e12458d1c56b620bf0452639260e}{PubMed})
+#'
+#' @source \href{https://i.redd.it/fd0f3p43mqqy.jpg}{Archive}
+#'
+#' @examples
+#' data(dog_study)
+"dog_study"
